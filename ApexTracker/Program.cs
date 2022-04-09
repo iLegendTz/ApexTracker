0ee47ca18f0409abaaee483ba9edd51833e37521
iLegendTz <miguel.isaac@live.com>
2022-04-08 23:01:31 -0500

    Modificando la ventana principal a Configuration

--- ApexTracker/Program.cs
+++ ApexTracker/Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ApexTracker.View;
+using System;
 using System.Windows.Forms;
 
 namespace ApexTracker
@@ -16,7 +14,7 @@ namespace ApexTracker
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Configuration());
         }
     }
 }
